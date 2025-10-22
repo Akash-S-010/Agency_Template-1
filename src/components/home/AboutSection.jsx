c15fcf7804314e787fdde343fd100c19a14f0120
Akash S <akashspalloor@gmail.com>
2025-10-22 21:44:03 +0530

    updated

--- src/components/home/AboutSection.jsx
+++ src/components/home/AboutSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LiquidText from '../ui/LiquidText.jsx';
+import backgroundImage from '../../assets/LiquidText_Background.jpg';
 
 const AboutSection = () => {
     const texts = [
@@ -9,9 +10,12 @@ const AboutSection = () => {
         "Professional Service"
     ];
     return (
-        <div className='flex flex-col min-h-[70vh] items-center justify-center text-primary gap-8'>
+        <div
+            className='flex flex-col min-h-[60vh] items-center justify-center text-primary gap-8 bg-cover bg-center bg-no-repeat'
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
             <h1 className='text-md md:text-xl font-light text-white'>What We Offers.</h1>
-            <LiquidText texts={texts} className="font-black text-primary mb-32" />
+            <LiquidText texts={texts} className="font-black text-primary mb-16" />
         </div>
     )
 }
