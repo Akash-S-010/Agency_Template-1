bad7bbf78779a22db6b609e867df553eb503ebb4
Akash S <akashspalloor@gmail.com>
2025-10-25 11:48:47 +0530

    updated style

--- src/components/home/TextAnimeSection.jsx
+++ src/components/home/TextAnimeSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import LiquidText from '../ui/LiquidText.jsx';
-import backgroundImage from '../../assets/LiquidText_Background.jpg';
 
 const TextAnimeSection = () => {
     const texts = [
@@ -11,11 +10,10 @@ const TextAnimeSection = () => {
     ];
     return (
         <div
-            className='flex flex-col min-h-[60vh] items-center justify-center text-primary gap-8 bg-cover bg-center bg-no-repeat'
-            style={{ backgroundImage: `url(${backgroundImage})` }}
+            className='flex flex-col min-h-[60vh] items-center justify-center border-t border-b border-primary/10 text-primary gap-8 bg-cover bg-center bg-no-repeat'
         >
             <h1 className='text-md md:text-xl font-light text-white'>What We Offers.</h1>
-            <LiquidText texts={texts} className="font-black text-primary mb-16" />
+            <LiquidText texts={texts} className="font-medium text-primary mb-16" />
         </div>
     )
 }
