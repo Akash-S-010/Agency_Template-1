88a1964e517c2e798a431a9d70eedbd792c86c5f
Akash S <akashspalloor@gmail.com>
2025-12-02 13:12:07 +0530

    size fixed

--- src/components/ui/StickyContact.jsx
+++ src/components/ui/StickyContact.jsx
@@ -4,7 +4,7 @@ import { IoCall } from "react-icons/io5";
 
 const StickyContact = () => {
   return (
-    <div className="fixed bottom-5 md:bottom-10 right-2 md:right-10 z-50 flex md:space-x-2">
+    <div className="fixed bottom-5 md:bottom-10 right-5 md:right-10 z-50 flex space-x-2">
       <a
         href="tel:+971563648546"
         className="bg-white size-10 md:size-12 flex items-center justify-center rounded-full drop-shadow-xl hover:scale-110 transition-transform duration-300 cursor-pointer"
