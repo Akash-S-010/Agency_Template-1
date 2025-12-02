932e924f945209f9a99db8a6ee5f5cce7b8a91b9
Akash S <akashspalloor@gmail.com>
2025-12-02 12:37:13 +0530

    updated spaces

--- src/components/ui/StickyContact.jsx
+++ src/components/ui/StickyContact.jsx
@@ -4,19 +4,19 @@ import { IoCall } from "react-icons/io5";
 
 const StickyContact = () => {
   return (
-    <div className="fixed bottom-5 md:bottom-10 right-2 md:right-10 z-50 flex-col space-y-2">
+    <div className="fixed bottom-5 md:bottom-10 right-2 md:right-10 z-50 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
       <a
         href="tel:+971563648546"
         className="bg-white size-10 md:size-12 flex items-center justify-center rounded-full shadow-lg hover:scale-110 transition-transform duration-300 cursor-pointer"
       >
-        <IoCall size={22} className="text-primary"/>
+        <IoCall size={23} className="text-primary" />
       </a>
       <a
         href="https://wa.me/971563648546"
         target="_blank"
         className="bg-white size-10 md:size-12 flex items-center justify-center rounded-full shadow-lg hover:scale-110 transition-transform duration-300 cursor-pointer"
       >
-        <FaWhatsapp size={24} className="text-primary" />
+        <FaWhatsapp size={25} className="text-primary" />
       </a>
     </div>
   );
